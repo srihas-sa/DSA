8e80c2a1238d1c4fb5130f782a316d33a5901187
srihas <edupugantisrihas2003@gmail.com>
2024-08-24 17:59:05 +0530

    Create MatrixRotation90[Array_Med].java

---
+++ MatrixRotation90[Array_Med].java
@@ -0,0 +1,36 @@
+// This will be the optimal approach where we transpose array means row==.col col==>row then reverse individual row.
+//No extra space complexity
+
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int length=matrix.length; 
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix.length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+
+            }
+            
+            }
+            System.out.println(matrix[0][0]);
+
+            
+                for(int j=0;j<matrix.length;j++){
+                    int start=0;
+                    int end=(matrix[j].length)-1;
+                    while(start<=end){
+                        int temp=matrix[j][start];
+                        matrix[j][start]=matrix[j][end];
+                        matrix[j][end]=temp;
+                        start=start+1;
+                        end=end-1;
+                    }
+                }
+            
+            
+        
+        System.out.println(matrix);
+    }
+    }
